b44a5b90b423496e6578a5f6aeba284a45741d71
lornarthebreton <lornarthebreton@pop-os.localdomain>
2021-01-30 23:42:22 -0600

    filtered the dataset and streamlined the code

--- python_code/main.py
+++ python_code/main.py
@@ -1,33 +1,31 @@
 import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
 import os
 
+today=datetime.today().strftime('%Y-%m-%d')
 location = input("Location: ") # Location to be monitored
-location_population = float(input("Population (in thousands): "))*1000 # Population of the location
 limit_a = (0.88,15) #(Percentile of people no longer infectious,days since symptoms)
 limit_b = (0.95,20) 
 people_day=float(input("People you meet in a day:"))#People you meet in a day
 
-given_period=30
+given_period=1
 #Creating and filtering Dataframes
-df=pd.read_csv("data.csv")
-df2=df[['location_name','date','est_infections_mean']]
-df3=df2[df2.location_name==location]
-df3=df3.dropna()
+df=pd.read_csv("mexican_projections.csv")
+df2=df[df.location_name==location]
+df2=df2.dropna()
 
 #Calculation of the contagion vectors column
-most_recent=df3.est_infections_mean.rolling(limit_a[1]-1).sum()
-contagion_a=((df3.est_infections_mean.rolling(limit_b[1]-1).sum()-most_recent)
+most_recent=df2.est_infections_mean.rolling(limit_a[1]-1).sum()
+contagion_a=((df2.est_infections_mean.rolling(limit_b[1]-1).sum()-most_recent)
             *(1-limit_a[0]))
-contagion_b=((df3.est_infections_mean.rolling(limit_b[1]).sum()-contagion_a)
+contagion_b=((df2.est_infections_mean.rolling(limit_b[1]).sum()-contagion_a)
             *(1-limit_b[0]))
-df3['contagion_vectors']=most_recent+contagion_a+contagion_b
+df2['contagion_vectors']=most_recent+contagion_a+contagion_b
 
 #Calcutation of the period's contagion probability 
-df3['contagion_probability']=(df3['contagion_vectors']/location_population
+df2['contagion_probability']=(df2['contagion_vectors']/df2['population']
                             *people_day*given_period)
 
-print(df3)
-df3.plot(x='date',y='contagion_probability')
-plt.show()
+print(df2[df2.date==today]['contagion_probability'])
 
