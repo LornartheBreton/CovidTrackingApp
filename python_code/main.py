fe9d8facb76387da38d2e9a600b3572c53ba4333
Lornar the Breton <lornarthebreton@pop-os.localdomain>
2021-02-02 21:04:25 -0600

    export to json

--- python_code/main.py
+++ python_code/main.py
@@ -2,31 +2,40 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 import os
+import firebase_admin
+from firebase_admin import db,storage,credentials,firestore
+import json
 
-today=datetime.today().strftime('%Y-%m-%d')
+#The code below was used to test the formula locally
+"""
+today=datetime.today().strftime('%Y-%m-%d')#Get today's date
 location = input("Location: ") # Location to be monitored
+people_day=1#People you meet in a day
+"""
 limit_a = (0.88,15) #(Percentile of people no longer infectious,days since symptoms)
 limit_b = (0.95,20) 
-people_day=float(input("People you meet in a day:"))#People you meet in a day
+given_period=1#Number of days on the time period (1 is one day, 30 is 30 days)
 
-given_period=1
 #Creating and filtering Dataframes
 df=pd.read_csv("mexican_projections.csv")
-df2=df[df.location_name==location]
-df2=df2.dropna()
+df=df.dropna()
 
 #Calculation of the contagion vectors column
-most_recent=df2.est_infections_mean.rolling(limit_a[1]-1).sum()
-contagion_a=((df2.est_infections_mean.rolling(limit_b[1]-1).sum()-most_recent)
+most_recent=df.est_infections_mean.rolling(limit_a[1]-1).sum()
+contagion_a=((df.est_infections_mean.rolling(limit_b[1]-1).sum()-most_recent)
             *(1-limit_a[0]))
-contagion_b=((df2.est_infections_mean.rolling(limit_b[1]).sum()-contagion_a)
+contagion_b=((df.est_infections_mean.rolling(limit_b[1]).sum()-contagion_a)
             *(1-limit_b[0]))
-df2['contagion_vectors']=most_recent+contagion_a+contagion_b
+df['contagion_vectors']=most_recent+contagion_a+contagion_b
 
+#Uploading the Dataframe to the cloud
+df.to_json(r'data_with_contagion_vectors.json')
+#The below code was used to test the formula locally
+"""
 #Calcutation of the period's contagion probability 
 df2['contagion_probability']=(df2['contagion_vectors']/df2['population']
                             *people_day*given_period)
 
 ans = df2[df2.date==today]['contagion_probability'].values[0]
 print(ans)
-
+"""
